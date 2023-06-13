8d7f4a8238f6e0fc0ec2b9d33a570635b2b9a09a
lohit <lohitkolluri@gmail.com>
2023-06-14 01:22:40 +0530

    .

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '7d80bdca86msh034d8ff63b3ba84p1c132ejsn9884b01e15d5',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7d80bdca86msh034d8ff63b3ba84p1c132ejsn9884b01e15d5",
+    "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+const getWeather = (city) => {
+  cityName.innerHTML = city
+
+fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city ,options)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+
+    // cloud_pct.innerHTML = response.cloud_pct;
+    temp.innerHTML = response.temp;
+    feels_like.innerHTML = response.feels_like;
+    humidity.innerHTML = response.humidity;
+    min_temp.innerHTML = response.min_temp;
+    max_temp.innerHTML = response.max_temp;
+    wind_speed.innerHTML = response.wind_speed;
+    wind_degrees.innerHTML = response.wind_degrees;
+    sunrise.innerHTML = response.sunrise;
+    sunset.innerHTML = response.sunset;
+  })
+  .catch((err) => console.error(err));
 }
+submit.addEventListener("click", (e) =>{
+  e.preventDefault()
+  getWeather(city.value)
+} )
+
+getWeather("Nagpur")

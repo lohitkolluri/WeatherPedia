66b06177b0881a3ee976f07e5b08cfb85cd061f6
lohit <lohitkolluri@gmail.com>
2023-06-13 23:25:49 +0530

    .

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7d80bdca86msh034d8ff63b3ba84p1c132ejsn9884b01e15d5',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}

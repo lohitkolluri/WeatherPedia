7af59142e423222239254d597d685a4e05bf2cbe
lohit <lohitkolluri@gmail.com>
2023-06-14 01:37:57 +0530

    .

--- script.js
+++ script.js
@@ -16,11 +16,14 @@ fetch("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city ,op
 
     // cloud_pct.innerHTML = response.cloud_pct;
     temp.innerHTML = response.temp;
+    temp2.innerHTML = response.temp;
     feels_like.innerHTML = response.feels_like;
     humidity.innerHTML = response.humidity;
+    humidity2.innerHTML = response.humidity;
     min_temp.innerHTML = response.min_temp;
     max_temp.innerHTML = response.max_temp;
     wind_speed.innerHTML = response.wind_speed;
+    wind_speed2.innerHTML = response.wind_speed;
     wind_degrees.innerHTML = response.wind_degrees;
     sunrise.innerHTML = response.sunrise;
     sunset.innerHTML = response.sunset;
